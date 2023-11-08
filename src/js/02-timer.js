582f963135333004bd4a437ba09e13b1050c8662
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-11-08 21:49:42 +0200

    start task 02

---
+++ src/js/02-timer.js
@@ -0,0 +1,25 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  input: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.disabled = true;
+
+flatpickr(refs.input, options);
